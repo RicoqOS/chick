53d0a08d1d4da2f4d764a5e55c91abe67f56f7a9
RealHinome <acn@gravitalia.com>
2025-08-20 15:49:20 +0200

    Add GDT, improve IDT

---
+++ src/kernel/src/arch/x86_64/interrupts/mod.rs
@@ -0,0 +1,13 @@
+/// Global descriptor table.
+mod gdt;
+
+/// Interrupt descriptor table.
+mod idt;
+
+/// Push IDT to IDTR.
+pub fn load() {
+    gdt::load();
+    idt::IDT.load();
+    x86_64::instructions::interrupts::enable(); // switch cli to sti.
+    log::info!("interrupts initialized");
+}
