96bf3a638bd2fb1bbcf2f59c550012c1d228c0a5
RealHinome <acn@gravitalia.com>
2025-12-25 23:46:41 +0100

    Use stack over heap on kernel

--- src/kernel/src/scheduler/percore.rs
+++ src/kernel/src/scheduler/percore.rs
@@ -1,28 +1,34 @@
-use alloc::boxed::Box;
-use alloc::vec::Vec;
-use core::cell::UnsafeCell;
+use core::mem::MaybeUninit;
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+pub const MAX_CPUS: usize = 0;
 
 use crate::arch::cpuid;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct PerCore<T> {
-    cores: Box<[UnsafeCell<T>]>,
+    cores: [MaybeUninit<T>; MAX_CPUS],
+    initialized_count: AtomicUsize,
 }
 
 unsafe impl<T> Sync for PerCore<T> {}
 
 impl<T> PerCore<T> {
-    /// Create [`PerCore`] with `n` cores.
-    pub fn new_with<F>(n: usize, mut init: F) -> Self
+    /// Init one core on [`PerCore`].
+    fn init_core<F>(&self, cpu_id: usize, mut init: F)
     where
         F: FnMut() -> T,
     {
-        let mut v = Vec::with_capacity(n);
-        for _ in 0..n {
-            v.push(UnsafeCell::new(init()));
+        if cpu_id + 1 > MAX_CPUS {
+            log::warn!("core {cpu_id} outbound max cores ({MAX_CPUS})");
+            return;
         }
-        let cores = v.into_boxed_slice();
-        Self { cores }
+
+        unsafe {
+            let ptr = self.cores[cpu_id].as_ptr() as *mut T;
+            ptr.write(init());
+        }
+        self.initialized_count.fetch_add(1, Ordering::Release);
     }
 
     /// Create default `T` with `n` cores.
@@ -30,29 +36,36 @@ impl<T> PerCore<T> {
     where
         T: Default,
     {
-        Self::new_with(n, T::default)
+        let percore = Self {
+            cores: [const { MaybeUninit::uninit() }; MAX_CPUS],
+            initialized_count: AtomicUsize::new(0),
+        };
+        for i in 0..n {
+            percore.init_core(i, T::default);
+        }
+        percore
     }
 
     /// Unsafe access to a core scheduler.
     pub unsafe fn get_unsafe(&self, i: usize) -> &T {
-        unsafe { &*self.cores[i].get() }
+        unsafe { self.cores[i].assume_init_ref() }
     }
 
     /// Access to current core scheduler.
     pub fn get(&self) -> &T {
         let i = cpuid().try_into().unwrap_or(0);
-        unsafe { &*self.cores[i].get() }
+        unsafe { self.cores[i].assume_init_ref() }
     }
 
     /// Mutable access to current core scheduler.
     #[allow(clippy::mut_from_ref)]
     pub fn get_mut(&self) -> &mut T {
         let i = cpuid().try_into().unwrap_or(0);
-        unsafe { &mut *self.cores[i].get() }
+        unsafe { &mut *(self.cores[i].as_ptr() as *mut T) }
     }
 
-    /// Number of cores.
+    /// Number of cores in use.
     pub fn len(&self) -> usize {
-        self.cores.len()
+        self.initialized_count.load(Ordering::Relaxed)
     }
 }
