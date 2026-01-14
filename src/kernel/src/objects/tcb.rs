a388cd2ccda879f4898a8ba7f03abc021ab70b94
RealHinome <acn@gravitalia.com>
2026-01-14 23:23:55 +0100

    Add TCB queue and update MR indexes

--- src/kernel/src/objects/tcb.rs
+++ src/kernel/src/objects/tcb.rs
@@ -8,10 +8,20 @@ use crate::error::Result;
 use crate::objects::cnode::CNodeEntry;
 use crate::objects::{CapRaw, CapRef, ObjType};
 
-#[derive(Debug)]
-pub enum FaultInfo {
-    PageFault { addr: usize },
-    SyscallFault { syscall: usize },
+// Forward declaration for Endpoint to avoid circular dependency.
+pub struct EndpointPtr(pub *mut u8);
+
+/// IPC transfer details stored in blocked thread state.
+#[derive(Debug, Clone, Copy, Default)]
+pub struct IpcState {
+    /// Badge to transfer.
+    pub badge: usize,
+    /// Can grant capabilities.
+    pub can_grant: bool,
+    /// Can grant reply capability.
+    pub can_grant_reply: bool,
+    /// Is this a call (expects reply).
+    pub is_call: bool,
 }
 
 #[derive(Debug, Default, Copy, Clone, Eq, PartialEq)]
@@ -85,6 +95,19 @@ pub struct Tcb {
 
     /// Thread state.
     pub state: ThreadState,
+
+    /// Next TCB in endpoint queue.
+    pub ep_next: Option<NonNull<Tcb>>,
+    /// Previous TCB in endpoint queue.
+    pub ep_prev: Option<NonNull<Tcb>>,
+
+    /// Object this thread is blocked on (endpoint pointer as raw).
+    pub blocking_object: Option<NonNull<u8>>,
+    pub ipc_state: IpcState,
+    /// When `BlockedOnReply` TCB we're waiting for reply from.
+    pub reply_to: Option<NonNull<Tcb>>,
+    /// TCB calling and waiting for reply.
+    pub caller: Option<NonNull<Tcb>>,
 }
 
 #[derive(Debug)]
@@ -117,6 +140,17 @@ impl Tcb {
             fault: None,
             fault_ep: CNodeEntry::new(),
             state: ThreadState::Running,
+            ep_next: None,
+            ep_prev: None,
+            blocking_object: None,
+            ipc_state: IpcState {
+                badge: 0,
+                can_grant: false,
+                can_grant_reply: false,
+                is_call: false,
+            },
+            reply_to: None,
+            caller: None,
         }
     }
 
@@ -136,18 +170,18 @@ impl Tcb {
 
 #[cfg(target_arch = "x86_64")]
 impl Tcb {
-    // RDI.
-    pub const MR1: usize = 5;
-    // RSI.
-    pub const MR2: usize = 4;
-    // RDX.
-    pub const MR3: usize = 3;
-    // RCX.
-    pub const MR4: usize = 2;
+    // RDI. Capacity badge.
+    pub const MR1: usize = 0;
+    // RSI. Message info tag.
+    pub const MR2: usize = 1;
+    // R10.
+    pub const MR3: usize = 9;
     // R8.
-    pub const MR5: usize = 7;
+    pub const MR4: usize = 10;
     // R9.
-    pub const MR6: usize = 8;
+    pub const MR5: usize = 11;
+    // R15.
+    pub const MR6: usize = 12;
 }
 
 pub type TcbCap<'a> = CapRef<'a, Tcb>;
@@ -162,5 +196,106 @@ impl TcbCap<'_> {
     pub fn identify(&self, tcb: &mut Tcb) -> usize {
         tcb.set_mr(Tcb::MR1, self.cap_type() as usize);
         1
+    }
+}
+
+/// Queue of TCBs waiting on an endpoint.
+#[derive(Debug, Default, Clone, Copy)]
+pub struct TcbQueue {
+    pub head: Option<NonNull<Tcb>>,
+    pub tail: Option<NonNull<Tcb>>,
+}
+
+impl TcbQueue {
+    /// Create an empty [`TcbQueue`].
+    pub const fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+
+    /// Append a TCB to the queue.
+    ///
+    /// # Safety
+    /// Caller must ensure `tcb` is valid and not already in a queue.
+    pub unsafe fn append(&mut self, tcb: NonNull<Tcb>) {
+        let tcb_ptr = tcb.as_ptr();
+
+        // Clear TCB's queue pointers.
+        (*tcb_ptr).ep_next = None;
+        (*tcb_ptr).ep_prev = self.tail;
+
+        match self.tail {
+            Some(tail) => {
+                (*tail.as_ptr()).ep_next = Some(tcb);
+            },
+            None => {
+                self.head = Some(tcb);
+            },
+        }
+
+        self.tail = Some(tcb);
+    }
+
+    /// Remove and return the head of the queue.
+    ///
+    /// # Safety
+    /// Caller must ensure queue TCBs are valid.
+    pub unsafe fn dequeue_head(&mut self) -> Option<NonNull<Tcb>> {
+        let head = self.head?;
+        let head_ptr = head.as_ptr();
+
+        self.head = (*head_ptr).ep_next;
+
+        match self.head {
+            Some(new_head) => {
+                (*new_head.as_ptr()).ep_prev = None;
+            },
+            None => {
+                self.tail = None;
+            },
+        }
+
+        (*head_ptr).ep_next = None;
+        (*head_ptr).ep_prev = None;
+
+        Some(head)
+    }
+
+    /// Remove a specific TCB from the queue.
+    ///
+    /// # Safety
+    /// Caller must ensure `tcb` is in this queue.
+    pub unsafe fn remove(&mut self, tcb: NonNull<Tcb>) {
+        let tcb_ptr = tcb.as_ptr();
+        let prev: Option<NonNull<Tcb>> = (*tcb_ptr).ep_prev;
+        let next: Option<NonNull<Tcb>> = (*tcb_ptr).ep_next;
+
+        match prev {
+            Some(prev_ptr) => {
+                (*prev_ptr.as_ptr()).ep_next = next;
+            },
+            None => {
+                self.head = next;
+            },
+        }
+
+        match next {
+            Some(next_ptr) => {
+                (*next_ptr.as_ptr()).ep_prev = prev;
+            },
+            None => {
+                self.tail = prev;
+            },
+        }
+
+        (*tcb_ptr).ep_next = None;
+        (*tcb_ptr).ep_prev = None;
     }
 }
